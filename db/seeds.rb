1e1c9b736a2c3087060ac178c0d49286f7989530
vadzimkutsevolau <v.kutsevolau@gmail.com>
2021-11-02 23:25:02 +0300

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
-    password:              "foobar",
-    password_confirmation: "foobar",
-    admin: true)
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 
 99.times do |n|
 name  = Faker::Name.name
@@ -19,5 +22,7 @@ password = "password"
 User.create!(name:  name,
       email: email,
       password:              password,
-      password_confirmation: password)
+      password_confirmation: password,
+      activated: true,
+      activated_at: Time.zone.now)
 end
